f0bc18cf85660639a4f18e1e6ff9c5ba92cb00fd
wgq <565859867@qq.com>
2021-07-06 14:58:52 +0800

    20210706 1418. 点菜展示表 中等 哈希表

---
+++ leetcode/2021/July/0706_1418displayTable.java
@@ -0,0 +1,82 @@
+/*1418. 点菜展示表
+
+给你一个数组 orders，表示客户在餐厅中完成的订单，确切地说， orders[i]=[customerNamei,tableNumberi,foodItemi] ，其中 customerNamei 是客户的姓名，tableNumberi 是客户所在餐桌的桌号，而 foodItemi 是客户点的餐品名称。
+
+请你返回该餐厅的 点菜展示表 。在这张表中，表中第一行为标题，其第一列为餐桌桌号 “Table” ，后面每一列都是按字母顺序排列的餐品名称。接下来每一行中的项则表示每张餐桌订购的相应餐品数量，第一列应当填对应的桌号，后面依次填写下单的餐品数量。
+
+注意：客户姓名不是点菜展示表的一部分。此外，表中的数据行应该按餐桌桌号升序排列。
+
+
+
+示例 1：
+
+输入：orders = [["David","3","Ceviche"],["Corina","10","Beef Burrito"],["David","3","Fried Chicken"],["Carla","5","Water"],["Carla","5","Ceviche"],["Rous","3","Ceviche"]]
+输出：[["Table","Beef Burrito","Ceviche","Fried Chicken","Water"],["3","0","2","1","0"],["5","0","1","0","1"],["10","1","0","0","0"]]
+解释：
+点菜展示表如下所示：
+Table,Beef Burrito,Ceviche,Fried Chicken,Water
+3    ,0           ,2      ,1            ,0
+5    ,0           ,1      ,0            ,1
+10   ,1           ,0      ,0            ,0
+对于餐桌 3：David 点了 "Ceviche" 和 "Fried Chicken"，而 Rous 点了 "Ceviche"
+而餐桌 5：Carla 点了 "Water" 和 "Ceviche"
+餐桌 10：Corina 点了 "Beef Burrito"
+
+示例 2：
+
+输入：orders = [["James","12","Fried Chicken"],["Ratesh","12","Fried Chicken"],["Amadeus","12","Fried Chicken"],["Adam","1","Canadian Waffles"],["Brianna","1","Canadian Waffles"]]
+输出：[["Table","Canadian Waffles","Fried Chicken"],["1","2","0"],["12","0","3"]]
+解释：
+对于餐桌 1：Adam 和 Brianna 都点了 "Canadian Waffles"
+而餐桌 12：James, Ratesh 和 Amadeus 都点了 "Fried Chicken"
+
+示例 3：
+
+输入：orders = [["Laura","2","Bean Burrito"],["Jhon","2","Beef Burrito"],["Melissa","2","Soda"]]
+输出：[["Table","Bean Burrito","Beef Burrito","Soda"],["2","1","1","1"]]
+
+
+
+提示：
+
+    1 <= orders.length <= 5 * 10^4
+    orders[i].length == 3
+    1 <= customerNamei.length, foodItemi.length <= 20
+    customerNamei 和 foodItemi 由大小写英文字母及空格字符 ' ' 组成。
+    tableNumberi 是 1 到 500 范围内的整数。
+*/
+class Solution {//全靠哈希表
+    public List<List<String>> displayTable(List<List<String>> orders) {
+        List<List<String>> res = new ArrayList<>();
+        HashSet<String> food = new HashSet<>();
+        HashMap<String, HashMap<String, Integer>> table = new HashMap<>();
+        for(List<String> l:orders){
+            food.add(l.get(2));//添加食物
+            HashMap<String, Integer> cur = table.getOrDefault(l.get(1), new HashMap<String, Integer>());
+            cur.put(l.get(2), cur.getOrDefault(l.get(2), 0) + 1);
+            table.put(l.get(1), cur);
+        }
+        List<String> f = new ArrayList<String>(food);//食物列表
+        Collections.sort(f);
+        f.add(0, "Table");
+        for (Map.Entry<String, HashMap<String, Integer>> entry : table.entrySet()) {
+            List<String> li = new ArrayList<>();
+            li.add(entry.getKey());
+            HashMap<String, Integer> h = entry.getValue();
+            for(int i = 1; i < f.size(); i++){
+                String s = f.get(i);
+                String c = String.valueOf(h.getOrDefault(s, 0));
+                li.add(c);
+            }
+			res.add(li);
+		}
+        Collections.sort(res, new Comparator<List<String>>() {
+            @Override
+            public int compare(List<String> l1, List<String> l2) {
+                return Integer.valueOf(l1.get(0)).compareTo(Integer.valueOf(l2.get(0)));
+            }
+        });
+        res.add(0, f);
+        return res;
+    }
+}
