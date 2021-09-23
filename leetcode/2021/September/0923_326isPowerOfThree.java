0b309e5641ff23bd742ae24fa423666eff8db323
wgq <565859867@qq.com>
2021-09-23 10:02:25 +0800

    20210923 326. 3的幂 简单 试除法|数学

---
+++ leetcode/2021/September/0923_326isPowerOfThree.java
@@ -0,0 +1,46 @@
+/*
+326. 3的幂
+
+给定一个整数，写一个函数来判断它是否是 3 的幂次方。如果是，返回 true ；否则，返回 false 。
+
+整数 n 是 3 的幂次方需满足：存在整数 x 使得 n == 3x
+
+
+
+示例 1：
+
+输入：n = 27
+输出：true
+
+示例 2：
+
+输入：n = 0
+输出：false
+
+示例 3：
+
+输入：n = 9
+输出：true
+
+示例 4：
+
+输入：n = 45
+输出：false
+
+
+
+提示：
+
+    -231 <= n <= 231 - 1
+*/
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        while(n >= 3){
+            if(n % 3 == 0)
+                n = n / 3;
+            else
+                return false;
+        }
+        return n == 1;
+    }
+}
