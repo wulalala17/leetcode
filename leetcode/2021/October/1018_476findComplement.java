d47baa391afd4d57fd74df19f412796c0727be74
wgq <565859867@qq.com>
2021-10-18 10:32:20 +0800

    20211018 476. 数字的补数 简单 位运算

---
+++ leetcode/2021/October/1018_476findComplement.java
@@ -0,0 +1,38 @@
+/*
+476. 数字的补数
+
+给你一个 正 整数 num ，输出它的补数。补数是对该数的二进制表示取反。
+
+
+
+示例 1：
+
+输入：num = 5
+输出：2
+解释：5 的二进制表示为 101（没有前导零位），其补数为 010。所以你需要输出 2 。
+
+示例 2：
+
+输入：num = 1
+输出：0
+解释：1 的二进制表示为 1（没有前导零位），其补数为 0。所以你需要输出 0 。
+
+
+
+提示：
+
+    给定的整数 num 保证在 32 位带符号整数的范围内。
+    num >= 1
+    你可以假定二进制数不包含前导零位。
+    本题与 1009 https://leetcode-cn.com/problems/complement-of-base-10-integer/ 相同
+*/
+class Solution {
+    public int findComplement(int num) {
+        int t = num, c = 0;
+        while(t > 0){
+            t >>= 1;
+            c = (c << 1) + 1;
+        }
+        return num ^ c;
+    }
+}
