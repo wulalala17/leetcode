bdb4c9e17ba9fd34c55bf082a2345b701d9ada58
wgq <565859867@qq.com>
2021-11-21 11:33:14 +0800

    20211121 559. N 叉树的最大深度 简单 BFS

---
+++ leetcode/2021/November/1121_559maxDepth.java
@@ -0,0 +1,69 @@
+/*
+559. N 叉树的最大深度
+
+给定一个 N 叉树，找到其最大深度。
+
+最大深度是指从根节点到最远叶子节点的最长路径上的节点总数。
+
+N 叉树输入按层序遍历序列化表示，每组子节点由空值分隔（请参见示例）。
+
+
+
+示例 1：
+
+输入：root = [1,null,3,2,4,null,5,6]
+输出：3
+
+示例 2：
+
+输入：root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+输出：5
+
+
+
+提示：
+
+    树的深度不会超过 1000 。
+    树的节点数目位于 [0, 104] 之间。
+*/
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public int maxDepth(Node root) {
+        if(root == null)
+            return 0;
+        Queue<Node> q = new LinkedList<>();
+        Queue<Node> cur = new LinkedList<>();
+        int res = 0;
+        q.offer(root);
+        while(!q.isEmpty()){
+            Node n = q.poll();
+            for(Node n1 : n.children){
+                cur.offer(n1);
+            }
+            if(q.isEmpty()){
+                q = cur;
+                cur = new LinkedList<>();
+                res++;
+            }
+        }
+        return res;
+    }
+}
